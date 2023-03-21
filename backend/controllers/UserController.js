f3e5a536ac69f0505fe09f3ab48607f6edc5fb6f
rafaelluiis2315 <rafael.barreto@liferay.com>
2023-03-20 21:26:18 -0300

    Add function of register user

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,19 +1,44 @@
 const User = require("../models/User");
+
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const jwtSecret = process.env.JWT_SECRET;
 
 // Generate user token
 const generateToken = (id) => {
-    return jwt.sign({ id }, jwtSecret, {
-        expiresIn: "7d"
-    })
+    return jwt.sign({ id }, jwtSecret, { expiresIn: "7d" });
 }
 
 const register = async (req, res) => {
-    res.send("Registro");
-}
+    try {
+        const { name, email, password } = req.body;
 
-module.exports = {
-    register,
-}
+        const user = await User.findOne({ email });
+        if (user) {
+            return res.status(422).json({ errors: ["Por favor, utilize outro e-mail"] });
+        }
+
+        const salt = await bcrypt.getSalt();
+        const passwordHash = await bcrypt.hash(password, salt);
+
+        const newUser = await User.create({
+            name,
+            email,
+            password: passwordHash,
+        });
+
+        return res.status(201).json({
+            _id: newUser._id,
+            token: generateToken(newUser._id),
+        });
+    } catch (err) {
+        console.error(err);
+        return res.status(422).json({
+            errors: ["Houve um erro, por favor tente novamente mais tarde."],
+        });
+    }
+};
+
+
+
+module.exports = { register };
