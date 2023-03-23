5dd38c6c618a1f7a0c035ac075f3bbd54b8ee569
rafaelluiis2315 <rafael.barreto@liferay.com>
2023-03-23 09:54:06 -0300

    Add middlewares imageUpload

---
+++ backend/middlewares/imageUpload.js
@@ -0,0 +1,33 @@
+const multer = require("multer");
+const path = require("path");
+
+// Destination to store image
+const imageStore = multer.diskStorage({
+    destination: (req, file, cd) => {
+        let folder = ""
+
+        if (req.baseUrl.includes("user")) {
+            folder = "users"
+        } else if (req.baseUrl.includes("photos")) {
+            folder = "photos"
+        }
+
+        cd(null, `uploads/${folder}/`)
+    },
+    filename: (req, file, cd) => {
+        cd(null, Date.now() + path.extname(file.originalname))
+    }
+})
+
+const imageUpload = multer({
+    storage: imageStore,
+    fileFilter: (req, file, cd) => {
+        if (!file.originalname.match(/\.(pgn|jpg)$/)) {
+            return cd(new Error("Por favor, envie apenas png ou jpg"))
+        }
+
+        cd(null, true)
+    }
+})
+
+module.exports = { imageUpload };
