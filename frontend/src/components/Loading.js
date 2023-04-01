26efecbcc4845238e0ed25cd67d75dcf1f128f7b
rafaelluiis2315 <rafael.barreto@liferay.com>
2023-04-01 19:08:10 -0300

    Create comoponent Loading

---
+++ frontend/src/components/Loading.js
@@ -0,0 +1,11 @@
+import './Loading.css'
+
+const Loading = () => {
+    return (
+        <div className='container_loader'>
+            <div className="loader"></div>
+        </div>
+    )
+}
+
+export default Loading
