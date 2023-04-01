a13b75d4aba3f6d4e469cab25b3ef558385c2c34
rafaelluiis2315 <rafael.barreto@liferay.com>
2023-04-01 19:40:48 -0300

    Adjust Navbar whit new pages

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,8 +1,31 @@
 import './Navbar.css';
 import { NavLink, Link } from 'react-router-dom';
-import { BsSearch, BsHouseDoorFill, BsFillPersonFill, BsFillCameraFill } from 'react-icons/bs'
+import { BsSearch, BsHouseDoorFill, BsFillPersonFill, BsFillCameraFill, BsBoxArrowRight } from 'react-icons/bs';
+
+// Hooks 
+import { useState } from 'react';
+import { useAuth } from '../hooks/useAuth';
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+
+// Redux
+import { logout, reset } from '../slices/authSlice';
 
 const Navbar = () => {
+    const { auth } = useAuth();
+    const { user } = useSelector((state) => state.auth);
+
+    const navigate = useNavigate();
+
+    const dispatch = useDispatch();
+
+    const handleLogout = () => {
+        dispatch(logout());
+        dispatch(reset());
+
+        navigate("/login");
+    }
+
     return (
         <nav id='nav'>
             <Link to="/">ReactGram</Link>
@@ -11,21 +34,47 @@ const Navbar = () => {
                 <input type="text" />
             </form>
             <ul id='nav-links'>
-                <li>
-                    <NavLink to="/">
-                        <BsHouseDoorFill />
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to="/login">
-                        Entar
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to="/register">
-                        Cadastrar
-                    </NavLink>
-                </li>
+                {
+                    auth ? (
+                        <>
+                            <li>
+                                <NavLink to="/">
+                                    <BsHouseDoorFill />
+                                </NavLink>
+                            </li>
+                            {user && (
+                                <li>
+                                    <NavLink to={`/users/${user._id}`}>
+                                        <BsFillCameraFill />
+                                    </NavLink>
+                                </li>
+                            )}
+                            <li>
+                                <NavLink to="/profile">
+                                    <BsFillPersonFill />
+                                </NavLink>
+                            </li>
+                            <li>
+                                <span onClick={handleLogout}>
+                                    <BsBoxArrowRight />
+                                </span>
+                            </li>
+                        </>
+                    ) : (
+                        <>
+                            <li>
+                                <NavLink to="/login">
+                                    Entar
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/register">
+                                    Cadastrar
+                                </NavLink>
+                            </li>
+                        </>
+                    )
+                }
             </ul>
         </nav>
     )
