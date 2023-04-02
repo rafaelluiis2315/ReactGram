5637fb54f83453378d2dfa8142bf6c40fe96a3fe
rafaelluiis2315 <rafael.barreto@liferay.com>
2023-04-02 14:33:37 -0300

    Fix photo validating when title not present

--- backend/middlewares/photoValidation.js
+++ backend/middlewares/photoValidation.js
@@ -1,13 +1,13 @@
 const { body } = require("express-validator");
 
-const photoInsertValidation  = () => {
+const photoInsertValidation = () => {
     return [
         body("title")
             .not().equals("undefined").withMessage("O título e obrigatório.")
-            .isString().withMessage("O título e obrigatório.")
-            .isLength({min: 3}).withMessage("O título precisa ter no mínimo 3 caracteres."),
+            .notEmpty().withMessage("O título e obrigatório.")
+            .isLength({ min: 3 }).withMessage("O título precisa ter no mínimo 3 caracteres."),
         body("image")
-            .custom((value, {req}) =>{
+            .custom((value, { req }) => {
                 if (!req.file) throw new Error("O campo image é obrigatório.");
 
                 return true;
@@ -15,16 +15,16 @@ const photoInsertValidation  = () => {
     ]
 }
 
-const photoUpdateValidation  = () => {
+const photoUpdateValidation = () => {
     return [
         body("title")
             .not().equals("undefined").withMessage("O título é obrigatório.")
             .isString().withMessage("O título é obrigatório.")
-            .isLength({min: 3}).withMessage("O título precisa ter no mínimo 3 caracteres."),
+            .isLength({ min: 3 }).withMessage("O título precisa ter no mínimo 3 caracteres."),
     ]
 }
 
-const commentValidation  = () => {
+const commentValidation = () => {
     return [
         body("comment")
             .not().equals("undefined").withMessage("O Comentário é obrigatório.")
