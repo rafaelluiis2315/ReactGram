ad601806a35461133a235fdbba35fc362830515c
rafaelluiis2315 <rafael.barreto@liferay.com>
2023-04-02 14:29:21 -0300

    Create Service and Slice get User Photos

--- frontend/src/slices/photoSlice.js
+++ frontend/src/slices/photoSlice.js
@@ -25,8 +25,19 @@ export const publishPhoto = createAsyncThunk("photo/publish",
     }
 );
 
+// Get user photos
+export const getUserPhotos = createAsyncThunk("photo/userPhotos",
+    async (id, thunkAPI) => {
+        const token = thunkAPI.getState().auth.user.token;
+
+        const data  = await photoService.getUserPhotos(id, token);   
+
+        return data;
+    }
+);
+
 export const photoSlice = createSlice({
-    name: "publish",
+    name: "photo",
     initialState,
     reducers: {
         resetMessage: (state) => {
@@ -51,6 +62,16 @@ export const photoSlice = createSlice({
                 state.loading = false;
                 state.error = action.payload;
                 state.photo = {};
+            })
+            .addCase(getUserPhotos.pending, (state) => {
+                state.loading = true;
+                state.error = false;
+            })
+            .addCase(getUserPhotos.fulfilled, (state, action) => {
+                state.loading = false;
+                state.success = true;
+                state.error = null;
+                state.photos = action.payload;
             });
     }
 })
