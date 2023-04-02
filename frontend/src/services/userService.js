4bdd831436ae556c5304c5129f479b9a84ea5289
rafaelluiis2315 <rafael.barreto@liferay.com>
2023-04-02 12:39:38 -0300

    Create Service and Slice get user details

--- frontend/src/services/userService.js
+++ frontend/src/services/userService.js
@@ -5,7 +5,7 @@ const profile = async (data, token) => {
     const config = requestConfig("GET", data, token);
 
     try {
-        
+
         const res = await fetch(api + "users/profile", config)
             .then((res) => res.json())
             .catch((err) => err);
@@ -21,7 +21,7 @@ const updateProfile = async (data, token) => {
     const config = requestConfig("PUT", data, token, true);
 
     try {
-        
+
         const res = await fetch(api + "users/", config)
             .then((res) => res.json())
             .catch((err) => err);
@@ -32,9 +32,25 @@ const updateProfile = async (data, token) => {
     }
 }
 
-const userService ={
+// Get user details 
+const getUserDetails = async (id) => {
+    const config = requestConfig("GET");
+
+    try {
+        const res = await fetch(api + "users/" + id, config)
+            .then((res) => res.json())
+            .catch((err) => err);
+
+        return res;
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+const userService = {
     profile,
     updateProfile,
+    getUserDetails,
 }
 
 export default userService;
