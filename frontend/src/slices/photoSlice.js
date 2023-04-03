2eb25d784363eaa434a63f63051a62b750ae4132
rafaelluiis2315 <rafael.barreto@liferay.com>
2023-04-03 09:52:10 -0300

    Create Service and Slice Delete Photo

--- frontend/src/slices/photoSlice.js
+++ frontend/src/slices/photoSlice.js
@@ -36,6 +36,21 @@ export const getUserPhotos = createAsyncThunk("photo/userPhotos",
     }
 );
 
+// Delete a photo
+export const deletePhoto = createAsyncThunk("photo/delete",
+    async (id, thunkAPI) => {
+        const token = thunkAPI.getState().auth.user.token;
+
+        const data  = await photoService.deletePhoto(id, token);
+
+        if (data.errors){   
+            return thunkAPI.rejectWithValue(data.errors[0])
+        }
+
+        return data;
+    }
+);
+
 export const photoSlice = createSlice({
     name: "photo",
     initialState,
@@ -72,6 +87,24 @@ export const photoSlice = createSlice({
                 state.success = true;
                 state.error = null;
                 state.photos = action.payload;
+            })
+            .addCase(deletePhoto.pending, (state) => {
+                state.loading = true;
+                state.error = false;
+            })
+            .addCase(deletePhoto.fulfilled, (state, action) => {
+                state.loading = false;
+                state.success = true;
+                state.error = null;
+                state.photos = state.photos.filter((photo) => {
+                    return photo._id !== action.payload.id
+                });
+                state.message = action.payload.message
+            })
+            .addCase(deletePhoto.rejected, (state, action) => {
+                state.loading = false;
+                state.error = action.payload;
+                state.photo = {};
             });
     }
 })
