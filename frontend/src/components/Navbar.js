1da708e91d11da1c6109ece649fa49a4b36207cd
rafaelluiis2315 <rafael.barreto@liferay.com>
2023-04-10 21:44:48 -0300

    implement functions NavBar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -15,6 +15,8 @@ const Navbar = () => {
     const { auth } = useAuth();
     const { user } = useSelector((state) => state.auth);
 
+    const [query, setQuery] = useState("");
+
     const navigate = useNavigate();
 
     const dispatch = useDispatch();
@@ -26,12 +28,20 @@ const Navbar = () => {
         navigate("/login");
     }
 
+    const handleSearch = (e) =>{
+        e.preventDefault()
+
+        if (query) {
+            return navigate(`/search?q=${query}`);
+        }
+    }
+
     return (
         <nav id='nav'>
             <Link to="/">ReactGram</Link>
-            <form id='search-form' >
+            <form id='search-form' onSubmit={handleSearch} >
                 <BsSearch />
-                <input type="text" />
+                <input type="text" placeholder='Pesquisar' onChange={(e) => setQuery(e.target.value)}/>
             </form>
             <ul id='nav-links'>
                 {
