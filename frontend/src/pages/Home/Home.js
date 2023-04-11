05dcedb640b09be515b0bfba31de792b0f75784a
rafaelluiis2315 <rafael.barreto@liferay.com>
2023-04-10 21:44:15 -0300

    implement functions home

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -1,8 +1,60 @@
-import React from 'react'
+import "./Home.css";
+
+// Components
+import LikeContainer from "../../components/LikeContainer";
+import PhotoItem from "../../components/PhotoItem";
+import Loading from "../../components/Loading"
+import { Link } from "react-router-dom";
+
+// Hooks
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useResetComponentMessage } from "../../hooks/useResetComponentMessage";
+
+// Redux
+import { getPhotos, like } from "../../slices/photoSlice";
 
 const Home = () => {
+    const dispatch = useDispatch();
+
+    const resetMessage = useResetComponentMessage(dispatch);
+
+    const { user } = useSelector((state) => state.auth);
+
+    const { photos, loading } = useSelector((state) => state.photo);
+
+    useEffect(() => {
+        dispatch(getPhotos())
+    }, [dispatch])
+
+    // like a photo
+    const handleLike = (photo) => {
+        dispatch(like(photo._id))
+
+        resetMessage()
+    }
+
+    if (loading) {
+        return <Loading />
+    }
+
     return (
-        <div>Home</div>
+        <div id="home">
+            {photos && photos.map((photo) => (
+                <div key={photo._id}>
+                    <PhotoItem photo={photo} />
+                    <LikeContainer photo={photo} user={user} handleLike={handleLike} />
+                    <Link className="btn" to={`/photos/${photo._id}`}>Ver mais</Link>
+                </div>
+            ))}
+
+            {photos && photos.length === 0 && (
+                <h2 className="no-photos">
+                    Ainda não há Fotos publicadas, <Link to={`/users/${user._id}`}>Publique a primeria Foto</Link>
+                </h2>
+            )}
+
+        </div>
     )
 }
 
